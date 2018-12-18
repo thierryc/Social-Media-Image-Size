768020f19bc5e35f7ad2da8f544c869e5eb0c09f
Thierry Charbonnel <thierryc@users.noreply.github.com>
2018-12-18 17:17:06 -0500

    initital commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _data = _interopRequireDefault(require("./data.json"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = _data.default;
+exports.default = _default;
